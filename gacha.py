4ce35518390af23064e5d36f3f44b68ea1a58271
dhaboav <dhaboav@gmail.com>
2023-05-25 02:18:49 +0700

    Memperbaiki dan menambahkan fitur informasi jumlah pull dan jumlah item yang didapatkan berdasarkan kategori

--- gacha.py
+++ gacha.py
@@ -1,7 +1,6 @@
 import random
 
 listKarakter6Star = ["All for One", "The Best Assassin", "Immortal One"]
-
 listKarakter5Star = ["Keqing", "Ayaka", "Ganyu", "Hu Tao", 
                      "Raiden", "Yoimiya", "Yae Miko", "Venti"
                      "Shenhe", "Xiao", "Yelan", "Zhongli"]
@@ -14,33 +13,44 @@ listItem3Star = ["Sword", "Spear", "Bow", "Gun",
                  "Shield", "Dagger", "Knife", "Ballon",
                  "Ball", "Shoes", "Jacket", "Hair"]
 
-bobot = [3] * len(listKarakter4Star) + [10] * len(listItem3Star) + [0.01] * len(listKarakter5Star)
-pilihan = listKarakter4Star + listItem3Star + listKarakter5Star
+bobot = [10] * len(listKarakter4Star) + [50] * len(listItem3Star) + [5] * len(listKarakter5Star) + [1] * len(listKarakter6Star)
+pilihan = listKarakter4Star + listItem3Star + listKarakter5Star + listKarakter6Star
 
-pity = 0
 def mesin_gacha():
-    global pity
-    if pity < 99:
-        if random.random() < 0.001:
-            pity = 0
-            return random.choice(listKarakter6Star)
-        else:
-            pity+=1
-            return random.choices(pilihan, weights=bobot, k=10)
-        
-    elif pity == 99:
-        pity = 0
-        return random.choice(listKarakter6Star)
-            
-    
-count6Star = 0
-other = 0
-for x in range(10):
-    gacha = mesin_gacha()
-    if gacha in listKarakter6Star:
-        count6Star+=1
-        print(gacha)
-    else:
-        other+=1
+    return random.choices(pilihan, weights=bobot, k=10)
 
-print(count6Star, other)
+
+hasil = []
+karakter6Star = 0
+karakter5Star = 0
+karakter4Star = 0
+item3Star = 0
+total = 0
+
+print("1 = 10 x pull\n")
+input_user = int(input("Masukan pull: "))
+if input_user >= 0 and input_user <= 1000:
+    for x in range(input_user):
+        gacha = mesin_gacha()
+        hasil.append(gacha)
+    
+    for x in range(len(hasil)):
+        for y in hasil[x]:
+            total +=1
+            if y in listKarakter6Star:
+                karakter6Star+=1
+            elif y in listKarakter5Star:
+                karakter5Star+=1
+            elif y in listKarakter4Star:
+                karakter4Star+=1
+            elif y in listItem3Star:
+                item3Star+=1
+    print(f"""
+Anda mendapatkan dari {total} pull:   
+{karakter6Star} karakter 6 star,
+{karakter5Star} karakter 5 star,
+{karakter4Star} karakter 4 star, dan
+{item3Star} item 3 star.               
+    """)
+else:
+    print("Pull dari 0 - 1000!")
